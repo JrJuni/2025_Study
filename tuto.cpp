6360236a70bfff8feeda417333a8545cbc4f4234
JrJuni <behomely0409@gmail.com>
2025-02-18 20:59:46 +0900

    add how to use git

--- tuto.cpp
+++ tuto.cpp
@@ -5,3 +5,14 @@ int main(){
     return 0;
 }
 
+/*
+git 복습
+0. git clone "repository address"
+0-1. git init
+0-1. git remote add origin "repository address"
+
+1. git pull origin main
+2. git add "filename.cpp"
+3. git commit -m "message"
+4. git push origin main
+*/
